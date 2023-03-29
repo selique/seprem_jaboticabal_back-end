8877317a43fc9c7123486501c3058f6622b82c28
Moisés Lopes Ferreira <judeudemolidor@hotmail.com>
2023-03-29 06:33:45 -0300

    Update codebase to reflect changes
    - Upgrade index.js, libs/extractPdfYearlyData.js
    - Upgrade package.json and package-lock.json
    - Remove redundant files from prisma folder
    - Delete beneficiary files from src folder

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 const express = require("express");
+const extractPdfData = require("./libs/extractPdfData");
+const extractPdfYearlyData = require("./libs/extractPdfYearlyData");
 const multer = require("multer");
 const { PDFDocument } = require("pdf-lib");
-const extractPdfData = require("./libs/extractPdfData");
-const cors = require("cors");
-const { createBeneficiary } = require("./src/beneficiary/createBeneficiary");
-const { uploadPdfBeneficiary } = require("./src/beneficiary/uploadPdfBeneficiary");
-const { existBeneficiary } = require("./src/beneficiary/existBeneficiary");
+const cors = require('cors');
+const pdf = require('pdf-extraction');
 
-const app = express();
-app.use(cors()); // Add this line to enable CORS
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  next();
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({
-  storage: multer.memoryStorage(),
-  limits: { fileSize: 1024 * 1024 * 10 },
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 50, // limit file size to 50MB
+  },
   fileFilter: (req, file, cb) => {
     if (file.mimetype !== "application/pdf") {
       cb(new Error("Invalid file type. Only PDF files are allowed."));
@@ -25,99 +20,135 @@ const upload = multer({
       cb(null, true);
     }
   },
-}).single("pdf");
+});
 
-app.post("/", (req, res) => {
-  upload(req, res, async (err) => {
-    if (err) {
-      console.error(`Error uploading file: ${err}`);
-      return res.status(500).send("Error uploading file");
-    }
 
-    const pdfData = req.file?.buffer;
-    if (!pdfData) {
-      return res.status(400).send("No file uploaded");
-    }
+const app = express();
 
-    const pdfDoc = await PDFDocument.load(pdfData);
-    const pageBuffers = [];
+// enable CORS
+app.use(cors());
 
-    try {
-      await Promise.all(
-        pdfDoc.getPages().map(async (_, index) => {
-          const newDocument = await PDFDocument.create();
-          const [copiedPage] = await newDocument.copyPages(pdfDoc, [index]);
-          newDocument.addPage(copiedPage);
-          const pageBuffer = await newDocument.save();
-          pageBuffers.push(pageBuffer);
-        })
-      );
-
-      const arrayResponseJson = [];
-
-      for (const pageBuffer of pageBuffers) {
-        try {
-          const extractedData = await extractPdfData(pageBuffer);
-          if (extractedData[0]) {
-            const { cpf, year, month, name, enrollment } = extractedData[0];
-            if (!cpf || !name || !enrollment) {
-              console.error("Invalid data extracted from PDF");
-              return res.status(400).send("Invalid input");
-            }
-
-            const pageFileName = `${cpf}-${name}-${enrollment}-${month}-${year}.pdf`;
-
-            if (!cpf) {
-              console.error("CPF is null or undefined");
-              return res.status(400).send("Invalid input");
-            }
-
-            const pdfObject = {
-              cpf,
-              name,
-              enrollment,
-              year,
-              month,
-              pdf: {
-                fileName: pageFileName,
-                file: Buffer.from(pageBuffer).toString("base64"),
-              },
-            };
-
-            // verify if beneficiary exists in database
-            const beneficiaryExists = await existBeneficiary(pdfObject);
-
-            if (beneficiaryExists) {
-              // upload pdf file into BeneficiaryPdfFile table
-              const result = await uploadPdfBeneficiary(pdfObject);
-
-              if (result.status !== 200) {
-                return res.status(result.status).json(result);
-              }
-            } else {
-              // create beneficiary
-              const result = await createBeneficiary(pdfObject);
-              if (result.status !== 200) {
-                return res.status(result.status).json(result);
-              }
-            }
-
-            arrayResponseJson.push(pdfObject);
-          }
-        } catch (error) {
-          console.error(`Error processing PDF: ${error}`);
-          return res.status(500).send(`Error processing PDF: ${error}`);
-        }
+app.post('/', (req, res) => {
+  upload.single("pdf")(req, res, async (err) => {
+    
+      if (err) {
+        console.error(`Error uploading file be: ${err}`);
+        return res
+          .writeHead(500, { "Content-Type": "text/plain" })
+          .end("Error uploading file");
       }
 
-      return res.status(200).json(arrayResponseJson);
-    } catch (error) {
-      console.error(`Error processing PDF: ${error}`);
-      return res.status(500).send("Error processing PDF");
-    }
+      const pdfData = req.file && req.file.buffer;
+      if (!pdfData) {
+        return res
+          .writeHead(400, { "Content-Type": "text/plain" })
+          .end("No file uploaded");
+      }
+
+      const pdfDoc = await PDFDocument.load(pdfData);
+      const pageBuffers = [];
+
+      try {
+        await Promise.all(
+          pdfDoc.getPages().map(async (_, index) => {
+            const newDocument = await PDFDocument.create();
+            const [copiedPage] = await newDocument.copyPages(pdfDoc, [index]);
+            newDocument.addPage(copiedPage);
+            const pageBuffer = await newDocument.save();
+            pageBuffers.push(pageBuffer);
+          })
+        );
+
+        const arrayResponseJson = [];
+
+        for (const pageBuffer of pageBuffers) {
+          if (req.query.fileType === "HOLERITE") {
+            try {
+              const extractedData = await extractPdfData(pageBuffer);
+
+              if (extractedData[0]) {
+                const { cpf, year, month, name, enrollment } = extractedData[0];
+                if (cpf && name && enrollment) {
+                  const pageFileName = `${cpf}-${name}-${enrollment}-${month}-${year}.pdf`;
+
+                  const nameWithoutDash = name.replace(/-/g, " ");
+                  const pdfObject = {
+                    name: nameWithoutDash,
+                    cpf,
+                    enrollment,
+                    year,
+                    month,
+                    pdf: {
+                      fileName: pageFileName,
+                      file: Buffer.from(pageBuffer).toString("base64"),
+                    },
+                  };
+                  arrayResponseJson.push(pdfObject);
+                } else {
+                  console.error("Invalid data extracted from PDF, Skipping...");
+                }
+              }
+            } catch (error) {
+              console.error(`Error processing PDF: ${error}`);
+              return res
+                .writeHead(500, { "Content-Type": "text/plain" })
+                .end(`Error processing PDF: ${error}`);
+            }
+          } else if (req.query.fileType === "DEMOSTRATIVO_ANUAL") {
+            try {
+              const extractedData = await extractPdfYearlyData(pageBuffer);
+              
+              if (extractedData[0]) {
+                const { cpf, name, year_current, year_calendar } = extractedData[0];
+                if (cpf && name && year_current && year_calendar) {
+                  const pageFileName = `${cpf}-${name}-${year_current}.pdf`;
+
+                  const nameWithoutDash = name.replace(/-/g, " ");
+                  const pdfObject = {
+                    cpf,
+                    name: nameWithoutDash,
+                    year: year_current,
+                    pdf: {
+                      fileName: pageFileName,
+                      file: Buffer.from(pageBuffer).toString("base64"),
+                    },
+                  };
+                  arrayResponseJson.push(pdfObject);
+                } else {
+                  console.log(extractedData[0])
+                  pdf(pageBuffer).then((data) => {
+                    console.log(data.text);
+                  });
+                  console.error("Invalid data extracted from PDF, Skipping...");
+                }
+              }
+            } catch (error) {
+              console.error(`Error processing PDF: ${error}`);
+              return res
+                .writeHead(500, { "Content-Type": "text/plain" })
+                .end(`Error processing PDF: ${error}`);
+            }
+          } else {
+            console.error("Invalid file type");
+            return res
+              .writeHead(400, { "Content-Type": "text/plain" })
+              .end("Invalid file type");
+          }
+        }
+
+        // return in json format the array of objects
+        res
+          .writeHead(200, { "Content-Type": "application/json" })
+          .end(JSON.stringify(arrayResponseJson));
+      } catch (error) {
+        console.error(`Error processing PDF: ${error}`);
+        res
+          .writeHead(500, { "Content-Type": "text/plain" })
+          .end("Error processing PDF");
+      }
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT ?? 3001, () => {
+  console.log(`Server started on port ${process.env.PORT ?? 3001}`);
 });
