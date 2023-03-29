c5ac71fbccc26b959668d6393a4c45d1dc7aa9c3
Moisés Lopes Ferreira <judeudemolidor@hotmail.com>
2023-03-29 08:27:42 -0300

    Refactor code to print extracted data and pdf text
    Add logging for extracting invalid data from PDF
    
    Rework code to log invalid data from PDF extraction
    rather than immediately print it to console

--- index.js
+++ index.js
@@ -116,10 +116,11 @@ app.post('/', (req, res) => {
                   };
                   arrayResponseJson.push(pdfObject);
                 } else {
-                  console.log(extractedData[0])
-                  pdf(pageBuffer).then((data) => {
-                    console.log(data.text);
-                  });
+               /* Printing the extracted data and the text from the pdf. */
+                  // console.log(extractedData[0])
+                  // pdf(pageBuffer).then((data) => {
+                  //   console.log(data.text);
+                  // });
                   console.error("Invalid data extracted from PDF, Skipping...");
                 }
               }
