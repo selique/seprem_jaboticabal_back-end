3e04e85c6a90fc5f2da86aaf1571c92c2248edc2
Moisés Lopes Ferreira <judeudemolidor@hotmail.com>
2023-04-12 08:49:26 -0300

    Refactor logic & update dependencies
    Refactor logic in index.js for better readability and
    update package-lock.json with latest dependencies.

--- index.js
+++ index.js
@@ -23,7 +23,11 @@ const upload = multer({
 const app = express();
 
 // enable CORS
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.post('/', upload.single("pdf"), async (req, res) => {
   const pdfData = req.file && req.file.buffer;
