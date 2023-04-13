42772a330638a16bd069e92738b0fe440807f9b5
Moisés Lopes Ferreira <judeudemolidor@hotmail.com>
2023-04-13 17:00:39 -0300

    Refactor: Change server port from 3000 to 8080
    Refactor server port from 3000 to 8080 to allow connection from outside the localhost.
    
    - Change `app.listen` port from `3000` to `8080`
    - Remove `Access-Control-Allow-Origin` header and replace it with `cors()` middleware

--- index.js
+++ index.js
@@ -23,11 +23,7 @@ const upload = multer({
 const app = express();
 
 // enable CORS
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 app.post('/', upload.single("pdf"), async (req, res) => {
   const pdfData = req.file && req.file.buffer;
@@ -146,6 +142,6 @@ app.post('/', upload.single("pdf"), async (req, res) => {
     }
 });
 
-app.listen(process.env.PORT ?? 3000, () => {
+app.listen(process.env.PORT ?? 8080, () => {
   console.log(`Server started on port ${process.env.PORT ?? 8080}`);
 });
