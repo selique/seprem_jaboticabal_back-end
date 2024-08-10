c19061a040dbb5e2b3bfcac55d0d51f244b25a3f
Moises Lopes Ferreira <moises@peakonedev.com>
2024-08-10 08:44:03 -0300

    fix: sanitize filename by removing unwanted symbols before validation
    - Updated the filename validation function to remove unwanted characters (e.g., special symbols) before applying the regex pattern.
    - Ensured that filenames containing only allowed characters (letters, numbers, hyphens, and dots) are validated correctly.
    - Added logging to indicate when a filename is skipped due to an invalid pattern.

--- libs/validationFileName.js
+++ libs/validationFileName.js
@@ -1,5 +1,16 @@
 const validateFilenamePattern = (filename) => {
+    // Remove símbolos do meio do nome (permitindo apenas letras, números, e hifens)
+    const sanitizedFilename = filename.replace(/[^A-Z0-9\-\.]/gi, '');
+
+    // Padrão atualizado para corresponder ao novo formato
     const pattern = /^\d{3}\.\d{3}\.\d{3}-\d{2}-[A-Z\-]+-\d+-\d{1,2}-\d{4}\.pdf$/;
-    return pattern.test(filename);
+
+    if (!pattern.test(sanitizedFilename)) {
+        console.log(`Invalid filename pattern. Skipping... ${filename}`);
+        return false;
+    }
+
+    return true;
 };
-module.exports = validateFilenamePattern
+
+module.exports = validateFilenamePattern;
